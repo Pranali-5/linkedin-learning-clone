92be652df523305fe1f2383e596e1d8deb5f06e7
Ankit Kumar <ankitkumar3443@gmail.com>
2021-08-28 19:12:49 +0530

    Cloud db setup

--- Project/src/db/conn.js
+++ Project/src/db/conn.js
@@ -4,7 +4,9 @@ const mongoose = require("mongoose");
 
 const connect = () => {
   return mongoose
-    .connect("mongodb://127.0.0.1:27017/lyndaRegistration", {
+    // .connect("mongodb://127.0.0.1:27017/lyndaRegistration", {
+    .connect("mongodb+srv://ankit_3443:ankit_3443%40@cluster0.j4oiz.mongodb.net/lyndaRegistration?retryWrites=true&w=majority", {
+
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
