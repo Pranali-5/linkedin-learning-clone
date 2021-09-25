c05b62c5c4bf9b8cc39455a27d2c9e9efd91c078
Ankit Kumar <ankitkumar3443@gmail.com>
2021-09-25 16:09:11 +0530

    Refactored App.js

--- Project/src/app.js
+++ Project/src/app.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-//const ejs = require("ejs");
+// const ejs = require("ejs");
 const connect = require("./db/conn.js");
 const checkoutController = require("./controllers/checkout.controller");
 const joinNow = require("./controllers/joinNow.controllers");
+const loginHandler = require("./controllers/login.controller");
 const Register = require("./models/registers.model.js");
 
 const port = process.env.PORT || 3000;
@@ -30,32 +31,16 @@ app.get(["/productsmahi1.html", "/productsmahi1"], async (req, res) => {
 
 //for registration
 app.use("/joinNow", joinNow);
-// app.get("/joinNow", async (req, res) => {
-//   res.render("HTML/joinNow");
-// });
 
 var e = "";
 //for login check
-app.post("/login", async (req, res) => {
-  try {
-    const email = req.body.email;
-    const password = req.body.password;
-    const useremail = await Register.findOne({ email: email });
-    if (useremail.password === password) {
-      e = useremail.firstName;
-      return res.status(201).render("HTML/checkout", {
-        firstName: useremail.firstName,
-      });
-    } else {
-    }
-  } catch (err) {}
-});
+app.post("/login", loginHandler);
 
-app.get("/login", async function (req, res) {
+/*app.get("/login", async function (req, res) {
   const users = await Register.find().lean().exec();
   // return res.send(users);
   return;
-});
+});*/
 
 app.get("/congrats", async function (req, res) {
   res.render("HTML/congrats");
@@ -67,7 +52,7 @@ app.get("/contact", async function (req, res) {
 
 app.get(["/allCourses", "/allCourses.html"], async function (req, res) {
   return res.render("HTML/allCourses", {
-    firstName: e,
+    firstName: 'Hi',
   });
 });
 
---
+++ Project/src/controllers/login.controller.js
@@ -0,0 +1,16 @@
+const login=async (req, res) => {
+    // var e;
+    try {
+      const email = req.body.email;
+      const password = req.body.password;
+      const useremail = await Register.findOne({ email: email });
+      if (useremail.password === password) {
+        e = useremail.firstName;
+        return res.status(201).render("HTML/checkout", {
+          firstName: useremail.firstName,
+        });
+      } 
+    } catch (err) {}
+  }
+
+module.exports = login;
